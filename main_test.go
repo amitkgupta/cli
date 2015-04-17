fe1321684e29b92c32e171f4a38ab792b23474f6
Amit Kumar Gupta <amitkgupta84@gmail.com>
2015-04-17 03:11:18 -0700

    Backfill tests for error handling bad pi input

--- main_test.go
+++ main_test.go
@@ -29,10 +29,22 @@ var _ = Describe("CLI", func() {
         session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
         Ω(err).ShouldNot(HaveOccurred())
 
+        Ω(session.Wait()).Should(gexec.Exit(0))
         resultBytes := session.Wait().Out.Contents()
         resultStr := strings.TrimSpace(string(resultBytes))
         result, err := strconv.ParseFloat(resultStr, 64)
         Ω(err).ShouldNot(HaveOccurred())
         Ω(result).Should(BeNumerically("~", math.Pi, 0.0001))
+    })
+
+    It("handles bad input", func() {
+        command := exec.Command(pathToCLI, "pi", "-n", "-1")
+        session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+        Ω(err).ShouldNot(HaveOccurred())
+
+        Ω(session.Wait()).Should(gexec.Exit(1))
+        errBytes := session.Wait().Err.Contents()
+        errString := strings.TrimSpace(string(errBytes))
+        Ω(errString).Should(ContainSubstring("Invalid n '-1'"))
     })
 })
